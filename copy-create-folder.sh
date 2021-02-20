63a310bdb6fa81a57d580eca10718af58fa987f5
code-jay <52211654+code-jay@users.noreply.github.com>
2021-02-20 08:55:11 +0530

    Create copy-create-folder.sh

---
+++ copy-create-folder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+srcdir="path/folder1"
+dstdir="path/folder2"
+
+for srcfile in ${srcdir}/*
+do
+    srcfile2=$(basename $srcfile)
+    folder_name=$(cut -d'_' -f1 <<<"$srcfile2")
+    dest_folder=$dstdir/$folder_name
+    if [ ! -d "$dest_folder" ] 
+    then
+        mkdir -p "$dest_folder"
+    fi
+    cp $srcfile $dest_folder    
+done
